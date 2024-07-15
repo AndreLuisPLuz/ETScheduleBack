ec9d70d3dc440e9fc0254a125ce8e565a52a6559
akda007 <Juandiego.jd42@gmail.com>
2024-07-15 11:04:12 -0300

    creating models

---
+++ src/main/java/ets/schedule/models/Users.java
@@ -0,0 +1,18 @@
+package ets.schedule.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class Users {
+    
+}
