e451e1519f7b6e08ca4bc35063856984ddff621c
AndreLuisPLuz <andreluispluz@gmail.com>
2024-07-16 11:34:12 -0300

    feat: application exception handler implemented.

---
+++ src/main/java/ets/schedule/filters/ApplicationExceptionHandler.java
@@ -0,0 +1,19 @@
+package ets.schedule.filters;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import ets.schedule.Exceptions.ApplicationException;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = { ApplicationException.class })
+    protected ResponseEntity<Object> handleConflict(
+            ApplicationException ex, WebRequest request
+    ) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
+    }
+}
