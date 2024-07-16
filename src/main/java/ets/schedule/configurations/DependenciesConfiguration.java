6c35d598cc653e63382f6d933a5d50a3f89abb4a
AndreLuisPLuz <andreluispluz@gmail.com>
2024-07-16 11:28:58 -0300

    feat: authentication dependencies configurated.

--- src/main/java/ets/schedule/configurations/DependenciesConfiguration.java
+++ src/main/java/ets/schedule/configurations/DependenciesConfiguration.java
@@ -1,8 +1,41 @@
 package ets.schedule.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.annotation.RequestScope;
+
+import ets.schedule.interfaces.services.AuthService;
+import ets.schedule.interfaces.services.PasswordService;
+import ets.schedule.interfaces.services.UserService;
+import ets.schedule.services.Auth0JwtService;
+import ets.schedule.services.DefaultUserService;
+import ets.schedule.services.PBKDF2PasswordService;
+import ets.schedule.sessions.UserSession;
 
 @Configuration
 public class DependenciesConfiguration {
+    @Bean
+    @Scope("singleton")
+    PasswordService passwordService() {
+        return new PBKDF2PasswordService();
+    }
 
+    @Bean
+    @Scope("singleton")
+    AuthService authService() {
+        return new Auth0JwtService();
+    }
+
+    @Bean
+    @RequestScope
+    UserSession userSession() {
+        return new UserSession();
+    }
+
+    @Bean
+    @Scope("singleton")
+    UserService userService() {
+        return new DefaultUserService();
+    }
 }
--- src/main/java/ets/schedule/interfaces/services/AuthService.java
+++ src/main/java/ets/schedule/interfaces/services/AuthService.java
@@ -2,7 +2,7 @@ package ets.schedule.interfaces.services;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import ets.schedule.data.payloads.LoginPayload;
+import ets.schedule.data.payloads.login.LoginPayload;
 import ets.schedule.data.responses.AuthResponse;
 
 public interface AuthService {
