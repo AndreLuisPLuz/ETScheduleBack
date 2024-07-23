75a1155cf40698283e003737a57a570077a35ce9
AndreLuisPLuz <andreluispluz@gmail.com>
2024-07-23 14:12:46 -0300

    fix: added preflight cors filter back to filters configuration.

--- src/main/java/ets/schedule/configurations/FiltersConfiguration.java
+++ src/main/java/ets/schedule/configurations/FiltersConfiguration.java
@@ -1,34 +1,49 @@
- package ets.schedule.configurations;
+package ets.schedule.configurations;
 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.boot.web.servlet.FilterRegistrationBean;
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
- import ets.schedule.filters.AuthFilter;
+import ets.schedule.filters.AuthFilter;
+import ets.schedule.filters.CorsPreflightFilter;
 
- @Configuration
- public class FiltersConfiguration {
-     @Autowired
-     AuthFilter authFilter;
+@Configuration
+public class FiltersConfiguration {
+    @Autowired
+    AuthFilter authFilter;
 
-     @Bean
-     @Scope("singleton")
-     protected FilterRegistrationBean<AuthFilter> registerAuthFilter() {
-         var registrationBean = new FilterRegistrationBean<AuthFilter>();
+    @Autowired
+    CorsPreflightFilter corsPreflightFilter;
 
-         registrationBean.setFilter(authFilter);
-         registrationBean.addUrlPatterns(
-             "/api/v1/user/*",
-             "/api/v1/course/*",
-             "/api/v1/discipline/*",
-             "/api/v1/event/*",
-             "/api/v1/group/*",
-             "/api/v1/report/*"
-         );
-         registrationBean.setOrder(1);
+    @Bean
+    @Scope("singleton")
+    protected FilterRegistrationBean<CorsPreflightFilter> registerCorsPreflightFilter() {
+        var registrationBean = new FilterRegistrationBean<CorsPreflightFilter>();
 
-         return registrationBean;
-     }
- }
+        registrationBean.setFilter(corsPreflightFilter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
+
+    @Bean
+    @Scope("singleton")
+    protected FilterRegistrationBean<AuthFilter> registerAuthFilter() {
+        var registrationBean = new FilterRegistrationBean<AuthFilter>();
+
+        registrationBean.setFilter(authFilter);
+        registrationBean.addUrlPatterns(
+                "/api/v1/user/*",
+                "/api/v1/course/*",
+                "/api/v1/discipline/*",
+                "/api/v1/event/*",
+                "/api/v1/group/*",
+                "/api/v1/report/*");
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
+}
